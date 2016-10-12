3b009c3d6d9583be6de5ac37e674bbce1a3bdde7
just-boris <just-boris@hotmail.com>
2016-10-12 22:57:20 +0200

    resolve loader issues

--- bin/styleguidist.js
+++ bin/styleguidist.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 const styleguidist = require('react-styleguidist');
-const exists = require('path-exists');
+const exists = require('path-exists').sync;
 const path = require('path');
 const script = process.argv[2];
 const components = process.argv[3];
 
+const title = require(path.resolve('package.json')).name;
+
 const guide = styleguidist({
+  title: title + ' – Styleguide',
   components,
   updateWebpackConfig(webpackConfig) {
     const sourceDir = path.resolve(__dirname, 'src');
@@ -14,16 +17,19 @@ const guide = styleguidist({
       webpackConfig.entry.push('./src/index.css');
     }
 
+    const babelLoader = craConfig.module.loaders.find(config => config.loader === 'babel');
+    craConfig.module.loaders.forEach(config => config.include = babelLoader.include);
+
     webpackConfig.module.loaders.push(...craConfig.module.loaders);
     return webpackConfig;
   }
-})
+});
 
 switch (script) {
 case 'build':
-  return guide.build();
+  return guide.build(e => e && console.error(e));
 case 'server':
-  return guide.server();
+  return guide.server(e => e && console.error(e));
 default:
   console.log('Unknown command "' + script + '".');
   process.exit(1);
